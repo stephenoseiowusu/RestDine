82bf5d319404474a75bf82587be0e48ab9ced433
Stephen <stephenoseiowusu199@gmail.com>
2017-03-03 13:49:56 -0500

    Add project files.

---
+++ RestDine/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RestDine.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ RestDine/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestDine.WebApiApplication" Language="C#" %>
